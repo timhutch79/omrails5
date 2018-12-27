72e1121a1fb1d084c36f71256f13936d38dd1046
timhutch79git config --global user.name tim.hutch.work@gmail.com <tim.hutch.work@gmail.com>
2018-12-27 14:08:33 +1000

    spafe place

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do 
+  		get "signin" => 'devise/sessions#new'
+  		delete "signout" => 'devise/sessions#destroy'
+  		get "signup" => 'devise/registrations#new'
+  	end
+	root 'pages#home'
+	get 'about' => 'pages#about'
+	get 'contactus' => 'pages#contactus'
+  get 'profile' => 'pages#profile'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
